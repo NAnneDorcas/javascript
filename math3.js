6964596d6f12e5fa4ab4c187497efd40d6badfff
Anne Dorcas <anne.dorcas.nguewouo.njamen@ut.ee>
2022-12-03 17:46:57 +0200

    file

---
+++ math3.js
@@ -0,0 +1,7 @@
+const radius=4 ;
+
+var  circumference=radius * PI * 2;
+var area = PI* (radius**2);
+
+console.log(Circumference);
+console.log(area);
---
+++ mathematical_operations.js
@@ -0,0 +1,9 @@
+function findDiagonal(s)
+{
+    return Math.sqrt(2) * s;
+}
+ 
+// Driver Code
+var S = 9;
+console.log(findDiagonal(S).toFixed(6));
+ 
